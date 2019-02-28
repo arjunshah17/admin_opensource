015cf59fb5b98cd0c4b62409372113de062ea2d0
arjun shah <arjun.18shah@gmail.com>
2019-02-28 13:01:55 +0530

    fix wishlist

--- app/src/main/java/com/example/agrify_admin/adapter/StoreAdapter.java
+++ app/src/main/java/com/example/agrify_admin/adapter/StoreAdapter.java
@@ -2,6 +2,7 @@ package com.example.agrify_admin.adapter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
@@ -16,13 +17,24 @@ import com.example.agrify_admin.R;
 import com.example.agrify_admin.databinding.ItemStoreProductBinding;
 import com.example.agrify_admin.model.Store;
 import com.example.agrify_admin.viewHolder.StoreHolder;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.WriteBatch;
+
+import es.dmoral.toasty.Toasty;
 
 public class StoreAdapter extends FirestoreAdapter<StoreHolder>  {
     Activity activity;
 
-
+WriteBatch batch;
+FirebaseFirestore firebaseFirestore;
 
     public interface OnStoreSelectedListener {
 
@@ -50,7 +62,8 @@ public class StoreAdapter extends FirestoreAdapter<StoreHolder>  {
         ItemStoreProductBinding itemStoreProductBinding = DataBindingUtil.
                 inflate(LayoutInflater.from(parent.getContext()), R.layout.item_store_product, parent, false);
 
-
+firebaseFirestore=FirebaseFirestore.getInstance();
+batch=firebaseFirestore.batch();
         return new StoreHolder(itemStoreProductBinding);
     }
 
@@ -62,9 +75,37 @@ public class StoreAdapter extends FirestoreAdapter<StoreHolder>  {
 
     public void delete(int pos)
     {
+        firebaseFirestore.collection("store").document(getSnapshot(pos).getId()).collection("wishlist").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentReference docRef;
+                    DocumentReference userProduct;
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.i("id",document.getId());
+                      docRef=firebaseFirestore.collection("wishlist").document(document.getId()).collection("wishlist").document(getSnapshot(pos).getId());
+                        Toasty.error(activity,document.getId(),Toasty.LENGTH_SHORT).show();
+                     batch.delete(docRef);
+                     userProduct=firebaseFirestore.collection("store").document(getSnapshot(pos).getId()).collection("wishlist").document(document.getId());
+                     batch.delete(userProduct);
+                    }
+                    batch.commit().addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            getSnapshot(pos).getReference().delete();
+                            notifyDataSetChanged();
+                        }
+                    });
 
-            getSnapshot(pos).getReference().delete();
-            notifyDataSetChanged();
+                } else {
+
+                    getSnapshot(pos).getReference().delete();
+                    notifyDataSetChanged();
+                }
+            }
+        });
+
+
 
     }
     public void edit(int pos)
